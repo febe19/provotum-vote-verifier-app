b6e9856bfd73ccf5808fbd14888b6ec4ae761271
febe19 <fabio.maddaloni@hispeed.ch>
2021-01-13 11:44:41 +0100

    Continuous Scanner
    Here:
    - Continuous scanner and division between challenge or Commitment.
    - New Scanner library

--- src/Components/Scanner.js
+++ src/Components/Scanner.js
@@ -1,99 +1,135 @@
-// https://github.com/kybarg/react-qr-scanner#readme
-
-import React, { Component } from 'react'
-import QRCodeScanner from './../HelperFunctions/QRScanner.js'
+import React, { useState } from 'react';
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 import { Link } from "react-router-dom";
 
-class Scanner extends Component {
-  constructor(props) {
-    super(props)
-    console.log(props.showScanner)
-    this.state = {
-      commitmentScanned: false,
-      commitment: null,
-      challenged: false,
-      challengeScanned: false,
-      challenge: null,
-      showScanner: true,
-      vote: false
+const Scanner = () => {
+
+  const [data, setData] = useState('');
+  const [ballotHash, setBallotHash] = useState('');
+  const [commitmentScanned, setCommitmentScanned] = useState(false);
+  const [challengeScanned, setChallengeScanned] = useState(false);
+  const [showScanner, setShowScanner] = useState(true);
+  var scannerHistory = [];
+  var qrData = {}
+  var publicKey = {}
+  var voterPublicKeyH = ''
+  var uniqueID = ''
+  var challenge = {}
+
+  const qrCodeIsCommitment = (qrData) => {
+    if ('id' in qrData && qrData.id === "Commitment") {
+      if ('Counter' in qrData && 'Total' in qrData && qrData.Counter <= qrData.Total) {
+        if (!('BH' in qrData) || !('publicKey' in qrData) || !('voterPublicKeyH' in qrData) || !('uniqueID' in qrData)) {
+          return false
+        } else {
+          setBallotHash(qrData.BH)
+          publicKey = qrData.publicKey
+          voterPublicKeyH = qrData.voterPublicKeyH
+          uniqueID = qrData.uniqueID
+          console.log("Commitment Scanned")
+          return true
+        }
+      } else {
+        return false
+      }
+    } else {
+      return false
     }
   }
 
-  handleScan = data => {
-    if (data.id === "Commitment" && !this.state.commitmentScanned) {
-      console.log("Scanned Commitment")
-      this.setState({
-        commitmentScanned: true,
-        commitment: data.data,
-        showScanner: false
-      })
-    }
-    else if (data.id === "Challenge" && !this.state.challengeScanned && this.state.commitmentScanned) {
-      console.log("Scanned Challenge")
-      this.setState({
-        challengeScanned: true,
-        challenge: data.data,
-        showScanner: false
-      })
-    }
-    else if (data.id === "Challenge" && !this.state.challengeScanned && !this.state.commitmentScanned) {
-      this.setState({
-        showScanner: false
-      })
+  const qrCodeIsChallenge = (qrData) => {
+    if ('id' in qrData && qrData.id === "Challenge") {
+      if ('Counter' in qrData && 'Total' in qrData && qrData.Counter <= qrData.Total) {
+        
+        if (typeof challenge[qrData.Counter] === 'undefined') {
+          challenge[qrData.Counter] = qrData
+          setData(JSON.stringify(challenge))
+          console.log("Added Challenge with Counter: "+ qrData.Counter)
+        } else {
+          console.log("Already Scanned Challenge with Counter: "+qrData.Counter)
+          return false
+        }
+        
+      } else {
+        return false
+      }
+    } else {
+      return false
     }
   }
 
-  handleError = err => {
-    console.error(err)
+  const handleScan = (err, result) => {
+    if (result) {
+      try {
+        qrData = JSON.parse(result.text)
+      } catch {
+        console.log("Could not parse JSON --> Result", result.text);
+        setData("Scanned QR code is not of expected format");
+        return;
+      }
+
+      if (!commitmentScanned && qrCodeIsCommitment(qrData)) {
+        setCommitmentScanned(true);
+        setShowScanner(false);
+
+      } else if (!challengeScanned && qrCodeIsChallenge(qrData)) {
+        setChallengeScanned(true)
+        setShowScanner(false)
+      } else {
+        console.log("QR Code is neither Commitment nor Challenge. Or one is already scanned")
+        return;
+      }
+
+      console.log("Parsed JSON: ", qrData)
+
+      scannerHistory.push(JSON.parse(result.text))
+    }
   }
 
-  onChallenge = () => {
-    this.setState({
-      challenged: true,
-      showScanner: true
-    })
+  const onChallenge = () => {
+    setShowScanner(true)
   }
 
-  render() {
-    return (
-      <div>
-        {!this.state.showScanner &&
-          <div>
-            {this.state.commitmentScanned && !this.state.challengeScanned &&
-              <div>
-                <p>You scanned the commitment. Continue with 'vote' or 'challenge'</p>
-                <Link to={{ pathname: '/result', scannerState: this.state, vote: true }} >
-                  <button disabled={this.state.showScanner} >Vote</button>
-                </Link>
-                <button disabled={this.state.showScanner} onClick={this.onChallenge}>Challenge</button>
-              </div>}
 
-            {this.state.commitmentScanned && this.state.challengeScanned &&
-              <div>
-                <p>You scanned the challenge. Continue with 'view challenge'</p>
-                <Link to={{ pathname: '/result', scannerState: this.state, vote: false }} >
-                  <button disabled={this.state.showScanner} >View Challenge</button>
-                </Link>
-              </div>}
-
-            {!this.state.commitmentScanned && !this.state.challengeScanned &&
-              <div>
-                <p>You must scan the commitment first</p>
-                <button disabled={this.state.showScanner} onClick={() => this.setState({ showScanner: true })}>Ok</button>
-              </div>}
+  return (
+    <>
+      {showScanner &&
+        <div>
+          {!commitmentScanned && <div>
+            Please scan the commitment
           </div>}
-
-        {this.state.showScanner &&
-          <div>
-            {this.state.commitment === null && this.state.challenge === null && <p>Scan the commitment</p>}
-            {this.state.challenge === null && this.state.commitment != null && <p>Scan the challenge</p>}
-            <QRCodeScanner onScan={this.handleScan} />
+          {!challengeScanned && commitmentScanned && <div>
+            Please scan the Challenge
           </div>}
+          <BarcodeScannerComponent
+            width={500}
+            height={500}
+            onUpdate={handleScan}
+          />
+        </div>
+      }
+      <p>Ballot Hash: {ballotHash}</p>
+      <p>Data: {data}</p>
 
-        <button onClick={() => console.log(this.state)}>show State</button>
-      </div >
-    )
-  }
+      {commitmentScanned && !showScanner && <div>
+        <p>You scanned the commitment. Continue with 'vote' or 'challenge'</p>
+        <Link to={{ pathname: '/result' }} >
+          <button>Vote</button>
+        </Link>
+        <button onClick={onChallenge}>Challenge</button>
+      </div>}
+
+      {commitmentScanned && challengeScanned &&
+        <div>
+          <p>You scanned the challenge. Continue with 'view challenge'</p>
+          <Link to={{ pathname: '/result' }} >
+            <button>View Challenge</button>
+          </Link>
+        </div>}
+
+
+    </>
+  )
 }
 
 export default Scanner;
